b5f61b7cd0608a241cb555e919b433d2c571383b
Dmitry Nekrasov <scannorone@gmail.com>
2017-05-04 17:14:21 +0700

    Тесты вычитания векторов, умножения вектора на число

--- src/Vector2DTest.java
+++ src/Vector2DTest.java
@@ -11,4 +11,22 @@ class Vector2DTest {
         Vector2D actual = vec1.add(vec2);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSubtract() {
+        Vector2D vec1 = new Vector2D(5, 11);
+        Vector2D vec2 = new Vector2D(6, 4);
+        Vector2D expected = new Vector2D(-1, 7);
+        Vector2D actual = vec1.subtract(vec2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        Vector2D vec1 = new Vector2D(3, 12);
+        int value = 7;
+        Vector2D expected = new Vector2D(21, 84);
+        Vector2D actual = vec1.multiply(value);
+        assertEquals(expected, actual);
+    }
 }
