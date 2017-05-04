650b6fb9317c0bfb7aeb314751aaed84226fda57
Dmitry Nekrasov <scannorone@gmail.com>
2017-05-04 09:47:07 +0700

    В класс Vector2D добавил операции сложения векторов, умножение на число

---
+++ src/Vector2D.java
@@ -0,0 +1,27 @@
+public class Vector2D {
+
+    private final int x;
+    private final int y;
+
+    public Vector2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2D add(Vector2D vec) {
+        int newX = x + vec.x;
+        int newY = y + vec.y;
+        return new Vector2D(newX, newY);
+    }
+
+    public Vector2D multiply(int val) {
+        int newX = x * val;
+        int nexY = y * val;
+        return new Vector2D(newX, nexY);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
