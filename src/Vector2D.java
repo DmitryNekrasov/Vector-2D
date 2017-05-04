2c38d849062cf5856c4a2d3e79e6a4fc660932c8
Dmitry Nekrasov <scannorone@gmail.com>
2017-05-04 15:33:22 +0700

    Векторное произведение, проверка на ортогональность и коллинеарность

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -3,9 +3,22 @@ public class Vector2D {
     private final int x;
     private final int y;
 
+    public Vector2D() {
+        this.x = 0;
+        this.y = 0;
+    }
+
     public Vector2D(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public Vector2D add(Vector2D vec) {
@@ -22,6 +35,18 @@ public class Vector2D {
 
     public int dotProduct(Vector2D vec) {
         return x * vec.x + y * vec.y;
+    }
+
+    public int crossProduct(Vector2D vec) {
+        return x * vec.y - vec.x * y;
+    }
+
+    public boolean isOrthogonal(Vector2D vec) {
+        return dotProduct(vec) == 0;
+    }
+
+    public boolean isColinear(Vector2D vec) {
+        return crossProduct(vec) == 0;
     }
 
     public double length() {
